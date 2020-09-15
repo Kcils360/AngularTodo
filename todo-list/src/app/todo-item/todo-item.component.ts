f9931aed4e0c6a4c73d0c7c68203207cfbabb14b
kcils360 <kcils360@gmail.com>
2020-09-15 13:40:41 -0700

    completed 18, added deletItem

--- todo-list/src/app/todo-item/todo-item.component.ts
+++ todo-list/src/app/todo-item/todo-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { TodoItem } from '../interfaces/todo-item';
 
 @Component({
@@ -6,6 +6,7 @@ import { TodoItem } from '../interfaces/todo-item';
   template: `
   <div class="todo-item">
     {{ item.title }}
+    <button class="btn btn-red" (click)="removeItem()">remove</button>
   </div>
   `,
   styleUrls: ['./todo-item.component.css']
@@ -13,10 +14,15 @@ import { TodoItem } from '../interfaces/todo-item';
 export class TodoItemComponent implements OnInit {
 
   @Input() item: TodoItem;
+  @Output() remove: EventEmitter<TodoItem> = new EventEmitter();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  removeItem() {
+    this.remove.emit(this.item);
   }
 
 }
