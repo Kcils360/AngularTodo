7e225d5c068b3058c9b61f38ccf2ef462b4bbad4
Kcils360 <dukes.gregoryd@gmail.com>
2020-09-14 18:34:49 -0700

    finished module 5

--- todo-list/src/app/input-button-unit/input-button-unit.component.ts
+++ todo-list/src/app/input-button-unit/input-button-unit.component.ts
@@ -10,10 +10,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./input-button-unit.component.css']
 })
 export class InputButtonUnitComponent implements OnInit {
-  title = 'Hello World';
-  constructor() { }
-
-  ngOnInit(): void {
+  title = 'nOta Button';
+  constructor() {
+    this.changeTitle('constructor-title');
   }
 
+  ngOnInit(): void {
+    // this.title = 'init-title';
+    this.changeTitle('init-title');
+  }
+
+  changeTitle(newTitle: string) {
+    this.title = newTitle;
+  }
 }
